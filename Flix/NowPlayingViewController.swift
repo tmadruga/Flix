1a9f1269c44a9dc6b21534683331fd617a1026b1
Tiffany Madruga <tmadruga@fb.com>
2017-06-21 17:22:26 -0700

    Added user story 3; added pull to refresh feature

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -17,15 +17,29 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     var movies: [[String: Any]] = []
 
-    
+    var refreshControl: UIRefreshControl!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        tableView.dataSource = self
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
+        tableView.dataSource = self
+        fetchMovies()
+        
+        
+        
+    }
+    
+    func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -41,13 +55,13 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
                 
-                }
             }
-    
+        }
+        
         
         task.resume()
-        
     }
 
     
