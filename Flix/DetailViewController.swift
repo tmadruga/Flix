02467fe977e9ea43d442d7069239a918a030140b
Tiffany Madruga <tmadruga@fb.com>
2017-06-22 16:18:10 -0700

    Added user story 4; created details view

---
+++ Flix/DetailViewController.swift
@@ -0,0 +1,69 @@
+//
+//  DetailViewController.swift
+//  Flix
+//
+//  Created by Tiffany Madruga on 6/22/17.
+//  Copyright © 2017 Tiffany Madruga. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+//Outlets
+    
+    @IBOutlet weak var backdropImageView: UIImageView!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    var movie: [String:Any]?
+    
+    
+    
+//Start
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    //passing in 
+        
+        if let movie = movie {
+            titleLabel.text = movie["title"] as? String
+            releaseDateLabel.text = movie["release_date"] as? String
+            overviewLabel.text = movie["overview"] as? String
+            let backdropPathString = movie["backdrop_path"] as! String
+            let posterPathString = movie["poster_path"] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backdropURL = URL(string: baseURLString + backdropPathString)!
+            backdropImageView.af_setImage(withURL: backdropURL)
+            
+            let posterpathURL = URL(string: baseURLString + posterPathString)!
+            posterImageView.af_setImage(withURL: posterpathURL)
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
