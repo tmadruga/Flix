2ae3e59a7a017e99c503178757c6c63689ec0810
Tiffany Madruga <tmadruga@fb.com>
2017-06-21 17:05:34 -0700

    Adding required user story 2; added movie title, descriptions, and posters.

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Tiffany Madruga on 6/21/17.
+//  Copyright © 2017 Tiffany Madruga. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
