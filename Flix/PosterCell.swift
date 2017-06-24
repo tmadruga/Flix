818abbd32999a895489155fc2523a137f09023f1
Tiffany Madruga <tmadruga@fb.com>
2017-06-23 18:04:46 -0700

    Added collection view to superheroes tab

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,16 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Tiffany Madruga on 6/23/17.
+//  Copyright © 2017 Tiffany Madruga. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var PosterImageView: UIImageView!
+
+    
+}
