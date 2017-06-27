4381973b78033f54162e62d1f6df5618a351ccb5
Tiffany Madruga <tmadruga@fb.com>
2017-06-27 00:05:23 -0700

    Finished collection view so that movies are lined up two in a row

--- Flix/SuperheroViewController.swift
+++ Flix/SuperheroViewController.swift
@@ -18,7 +18,21 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
         super.viewDidLoad()
 
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width/cellsPerLine - interItemSpacingTotal/cellsPerLine
+        
+        layout.itemSize = CGSize(width: width, height: width*3/2)
+        
+        
+        
         fetchMovies()
+        
+        
         // Do any additional setup after loading the view.
     }
 
